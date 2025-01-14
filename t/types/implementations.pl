0d248a10d2068342e04398914691e0eb923ed0f4
brain-fuel <matt@brain-fuel.co>
2025-01-14 12:00:28 -0500

    initial attempt at coming up with boolean operators

--- t/types/implementations.pl
+++ t/types/implementations.pl
@@ -1,6 +1,24 @@
-implements_trait(bool, un_op, bool_null).
-implements_trait(bool, un_op, bool_id).
-implements_trait(bool, un_op, bool_transfer).
-implements_trait(bool, un_op, bool_not).
+%% Need to figure out how to deal with the conflict between boolean id function and unital magma id function (identity property)
+implements_trait(bool, un_op, bool_null_un). % null
+implements_trait(bool, un_op, bool_id_un). % id
+implements_trait(bool, un_op, bool_transfer). % tx
+implements_trait(bool, un_op, bool_not). % not
 
-implements_trait(bool, bin_op, bool_nand).
+implements_trait(bool, bin_op, bool_null_bin). % |null|
+implements_trait(bool, bin_op, bool_id_bin). % |id|
+implements_trait(bool, bin_op, bool_left). % |left|
+implements_trait(bool, bin_op, bool_right). % |right|
+implements_trait(bool, bin_op, bool_not_left). % |!left|
+implements_trait(bool, bin_op, bool_not_right). % |!right|
+implements_trait(bool, bin_op, bool_nand). % |nand|
+implements_trait(bool, bin_op, bool_and). % |and|
+implements_trait(bool, bin_op, bool_nor). % |nor|
+implements_trait(bool, bin_op, bool_or). % |or|
+implements_trait(bool, bin_op, bool_xnor). % |xnor|
+implements_trait(bool, bin_op, bool_xor). % |xor|
+implements_trait(bool, bin_op, bool_left_implies_right). % |<-|
+implements_trait(bool, bin_op, bool_right_implies_left). % |->|
+implements_trait(bool, bin_op, bool_left_inhibits_right). % |<-/|
+implements_trait(bool, bin_op, bool_right_inhibits_left). % |\->|
+implements_trait(bool, bin_op, bool_iff). % |<->|
+
